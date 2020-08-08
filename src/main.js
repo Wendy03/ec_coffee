6a8f4cc6b7472ccd5f2bdaefcf386e608d37ca57
wendy03 <wendysu03@gmail.com>
2020-08-08 14:44:35 +0800

    Event bus

--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import 'bootstrap';
+import {
+  ValidationObserver,
+  ValidationProvider, configure, localize, extend,
+} from 'vee-validate'; // 驗證套件
+import * as rules from 'vee-validate/dist/rules'; // 規則檔案（ex: email...）
+import zhTW from 'vee-validate/dist/locale/zh_TW.json'; // 語系檔案
+import './bus';
 import App from './App.vue';
 import router from './router';
+import dateFilter from './filters/date';
+import moneyFilter from './filters/money';
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
+Object.keys(rules).forEach((rule) => {
+  extend(rule, rules[rule]);
+}); // 所有驗證規則
+configure({
+  classes: {
+    valid: 'is-valid',
+    invalid: 'is-invalid',
+  },
+});
+localize('tw', zhTW);
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
+
+Vue.component('Loading', Loading);
+
+Vue.filter('date', dateFilter);
+Vue.filter('money', moneyFilter);
 
 new Vue({
   router,
