e26ca4d2c5a85f040b086bff320270e31cfe7311
wendy03 <wendysu03@gmail.com>
2020-08-09 01:50:41 +0800

    fix UI

--- src/components/ToTop.vue
+++ src/components/ToTop.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="text-right mb-1"
+  <div class="text-right mb-1 mr-1"
+       style="padding-bottom: 80px"
        @click.prevent="totop">
     <i class="fas fa-chevron-up border border-dark rounded-circle p-2"></i>
   </div>
