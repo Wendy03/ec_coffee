fd8242479499b2a5e474eb7f90e10077ddbdcab5
wendy03 <wendysu03@gmail.com>
2020-08-08 14:40:22 +0800

    to top

---
+++ src/components/ToTop.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-right mb-1"
+       @click.prevent="totop">
+    <i class="fas fa-chevron-up border border-dark rounded-circle p-2"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    totop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    },
+  },
+};
+</script>
