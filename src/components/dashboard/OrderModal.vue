64d8ea58297b33210c023609d3e2f670578e0e4b
wendy03 <wendysu03@gmail.com>
2020-08-08 14:35:57 +0800

    Admin Orders page

---
+++ src/components/dashboard/OrderModal.vue
@@ -0,0 +1,107 @@
+<template>
+  <!-- Order Modal -->
+  <div class="modal fade"
+       id="orderModal"
+       tabindex="-1"
+       role="dialog"
+       aria-labelledby="exampleModalLabel"
+       aria-hidden="true">
+    <div class="modal-dialog"
+         role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title"
+              id="exampleModalLabel">顧客資料</h5>
+          <button type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <table class="table">
+            <tbody>
+              <tr>
+                <th scope="row"
+                    class="font-weight-normal">Email</th>
+                <td>
+                  {{order.user.email}}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row"
+                    class="font-weight-normal">收貨人姓名</th>
+                <td>
+                  {{order.user.name}}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row"
+                    class="font-weight-normal">收貨人電話</th>
+                <td>
+                  {{order.user.tel}}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row"
+                    class="font-weight-normal">收貨人地址</th>
+                <td>
+                  {{order.user.address}}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row"
+                    class="font-weight-normal">留言</th>
+                <td>
+                  {{order.message}}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery';
+import Toast from '../../utils/Toast';
+
+export default {
+  data() {
+    return {
+      order: {
+        user: {},
+      },
+
+    };
+  },
+  props: {
+    status: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    getOrder(id) {
+      const url = `${process.env.VUE_APP_APIPATH}${process.env.VUE_APP_UUID}/admin/ec/orders/${id}`;
+      this.$http
+        .get(url)
+        .then((res) => {
+          this.order = res.data.data;
+          this.status.loadingItem = '';
+          $('#orderModal').modal('show');
+        })
+        .catch(() => {
+          this.status.loadingItem = '';
+          Toast.fire({
+            title: '無法取得資料，稍後再試',
+            icon: 'error',
+          });
+        });
+    },
+  },
+};
+</script>
---
+++ src/views/backend/Orders.vue
@@ -0,0 +1,158 @@
+<template>
+  <div>
+    <loading :active.sync="isLoading"></loading>
+    <div class="table-responsive"
+         v-if="orders.length > 0">
+      <table class="table table-striped mt-5">
+        <thead>
+          <tr>
+            <th width="140">購買時間</th>
+            <th>購買款項</th>
+            <th width="140">付款金額</th>
+            <th width="140">付款方式</th>
+            <th width="140">是否付款</th>
+            <th width="100">客戶資料</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in sortOrder"
+              :key="item.id">
+            <td>{{ item.created.timestamp | date}}</td>
+            <td>
+              <ul class="list-unstyled">
+                <li v-for="(product, i) in item.products"
+                    :key="i">
+                  {{ product.product.title }} 數量：{{ product.quantity
+                          }} {{ product.product.unit }}
+                </li>
+              </ul>
+            </td>
+            <td class="text-center">{{ item.amount | money}}</td>
+            <td class="text-center">{{ item.payment}}</td>
+            <td>
+              <div class="custom-control custom-switch">
+                <input :id="item.id"
+                       v-model="item.paid"
+                       type="checkbox"
+                       class="custom-control-input"
+                       @change="updatePayment(item)">
+                <label class="custom-control-label"
+                       :for="item.id">
+                  <strong v-if="item.paid"
+                          class="text-success">已付款</strong>
+                  <span v-else
+                        class="text-muted">尚未付款</span>
+                </label>
+              </div>
+            </td>
+            <td>
+              <button class="btn btn-outline-primary btn-sm mr-1"
+                      @click.prevent="openModal(item)">
+                <i class="fas fa-spinner fa-spin"
+                   v-if="item.id === status.loadingItem"></i>
+                查看
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- 分頁 -->
+      <Pagination :pages="pagination"
+                  @changePage="getOrders"></Pagination>
+
+      <!-- Order Modal -->
+      <OrderModal ref="orderModal" :status="status"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Toast from '../../utils/Toast';
+import Pagination from '../../components/Pagination.vue';
+import OrderModal from '../../components/dashboard/OrderModal.vue';
+
+export default {
+  data() {
+    return {
+      orders: [],
+      pagination: {},
+      order: {
+        user: {},
+      },
+      isLoading: true,
+      status: {
+        loadingItem: '',
+      },
+    };
+  },
+  components: {
+    Pagination,
+    OrderModal,
+  },
+  created() {
+    this.getOrders();
+  },
+  methods: {
+    getOrders(page = 1) {
+      const url = `${process.env.VUE_APP_APIPATH}${process.env.VUE_APP_UUID}/admin/ec/orders?page=${page}`;
+      this.isLoading = true;
+      this.$http
+        .get(url)
+        .then((res) => {
+          this.isLoading = false;
+          this.orders = res.data.data;
+          this.pagination = res.data.meta.pagination;
+        })
+        .catch(() => {
+          this.isLoading = false;
+          Toast.fire({
+            title: '無法取得資料，稍後再試',
+            icon: 'error',
+          });
+        });
+    },
+    updatePayment(item) {
+      let url = `${process.env.VUE_APP_APIPATH}${process.env.VUE_APP_UUID}/admin/ec/orders/${item.id}/paid`;
+      this.isLoading = true;
+      if (!item.paid) {
+        url = `${process.env.VUE_APP_APIPATH}${process.env.VUE_APP_UUID}/admin/ec/orders/${item.id}/unpaid`;
+      }
+      this.$http
+        .patch(url, item.id)
+        .then(() => {
+          this.isLoading = false;
+          Toast.fire({
+            title: '付款狀態修改成功',
+            icon: 'success',
+          });
+          this.getOrders();
+        })
+        .catch(() => {
+          this.isLoading = false;
+          Toast.fire({
+            title: '付款失敗',
+            icon: 'error',
+          });
+        });
+    },
+    openModal(item) {
+      this.$refs.orderModal.getOrder(item.id);
+      this.status.loadingItem = item.id;
+    },
+  },
+  computed: {
+    sortOrder() {
+      const vm = this;
+      let newOrder = [];
+      if (vm.orders.length) {
+        newOrder = vm.orders.sort((a, b) => {
+          const aIsPaid = a.is_paid ? 1 : 0;
+          const bIsPaid = b.is_paid ? 1 : 0;
+          return bIsPaid - aIsPaid;
+        });
+      }
+      return newOrder;
+    },
+  },
+};
+</script>
