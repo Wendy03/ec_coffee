41848247e01bf772e07b7d9b1b5776f5a23bd56e
wendy03 <wendysu03@gmail.com>
2020-08-08 14:39:58 +0800

    Footer

---
+++ src/components/front/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="container-fluid navbar-fixed-bottom bg-dark">
+    <div class="d-flex align-items-center justify-content-between text-white py-4">
+      <ul class="d-flex list-unstyled mb-0 h4">
+        <li><a href="#"
+             class="text-white mx-3"><i class="fab fa-facebook"></i></a></li>
+        <li><a href="#"
+             class="text-white"><i class="fab fa-line"></i></a></li>
+      </ul>
+      <p>無商業用途，僅供學習使用</p>
+      <router-link to="/admin/products"
+                   class="nav-link text-light">
+        <i class="fas fa-user-cog"></i>
+        商品管理
+      </router-link>
+    </div>
+  </footer>
+</template>
