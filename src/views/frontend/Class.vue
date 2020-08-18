525e96f29ce023ac59f5ed9f4a1af42c38c1dd8f
wendy03 <wendysu03@gmail.com>
2020-08-18 22:40:07 +0800

    修正細節

--- src/views/frontend/Class.vue
+++ src/views/frontend/Class.vue
@@ -8,7 +8,7 @@
           <div class="col-lg-4">
             <img src="https://images.unsplash.com/photo-1514927298007-a2b56e5270e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80"
                  alt=""
-                 class="w-100">
+                 class="img-fluid">
           </div>
           <div class="col-lg-6 mt-3">
             <h4>新鮮度判斷</h4>
@@ -25,7 +25,7 @@
           <div class="col-lg-4">
             <img src="https://images.unsplash.com/44/E5KMvPp9SsCnqmEGUwAS_2014-08-10%2012.56.40%201.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=959&q=80"
                  alt=""
-                 class="w-100">
+                 class="img-fluid">
           </div>
           <div class="col-lg-6 mt-3">
             <h4>咖啡豆保存</h4>
@@ -42,7 +42,7 @@
           <div class="col-lg-4">
             <img src="https://images.unsplash.com/photo-1514927298007-a2b56e5270e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80"
                  alt=""
-                 class="w-100">
+                 class="img-fluid">
           </div>
           <div class="col-lg-6 mt-3">
             <h4>手沖咖啡</h4>
